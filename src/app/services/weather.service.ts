a91241d58d18153920f05b79fc7b0e0befc74dca
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-22 18:02:15 +0400

    feat: add api requests

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,6 +5,7 @@ import { environment } from 'src/environments/environment';
 import { CurrentWeather } from '../interface/CurrentWeather';
 import { Forecast } from '../interface/Forecast';
 import { CityCoordinates } from '../interface/CityCoordinates';
+import { AirPollution } from '../interface/AirPollution';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class WeatherService {
   //private currentWeather: BehaviorSubject<ICurrentWeather> = new BehaviorSubject<ICurrentWeather>
   //currentWeather$: Observable<ICurrentWeather> = this.currentWeather.asObservable()
 
-  getCurrentWeatherByCoordinates(latitude: string, longitude: string, langShort: string): Observable<CurrentWeather> {
+  getCurrentWeatherByCoordinates(latitude: string, longitude: string, langShort: string = "en"): Observable<CurrentWeather> {
     return this.http.get<CurrentWeather>(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=${langShort}&units=metric&appid=${environment.openWeather.API_key}`);
   }
 
@@ -24,12 +25,32 @@ export class WeatherService {
     return this.http.get<CurrentWeather>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${environment.openWeather.API_key}`)
   }
 
-  getForecast(latitude: string, longitude: string, langShort: string): Observable<Forecast>{
+  getForecastByCoordinates(latitude: string, longitude: string, langShort: string = "en"): Observable<Forecast>{
     return this.http.get<Forecast>(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&lang=${langShort}&units=metric&appid=${environment.openWeather.API_key}`);
   }
 
-  getCoordinatesByLocationName(cityName: string, countryCode: string): Observable<CityCoordinates[]>{
-    return this.http.get<CityCoordinates[]>(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&appid=${environment.openWeather.API_key}`)
+  getForecastByCity(cityName: string): Observable<Forecast>{
+    return this.http.get<Forecast>(`api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${environment.openWeather.API_key}`);
+  }
+
+  getCoordinatesByLocationName(cityName: string, countryCode: string, limit: number = 1): Observable<CityCoordinates[]>{
+    return this.http.get<CityCoordinates[]>(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=${limit}&appid=${environment.openWeather.API_key}`)
+  }
+
+  getLocationNameByCoordinates(latitude: string, longitude: string, limit: number = 1 ): Observable<CityCoordinates[]>{
+    return this.http.get<CityCoordinates[]>(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=${limit}&appid=${environment.openWeather.API_key}}`)
+  }
+
+  getCurrentAirPollution(latitude: string, longitude: string): Observable<AirPollution>{
+    return this.http.get<AirPollution>(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${environment.openWeather.API_key}`)
+  }
+
+  getForecastAirPollution(latitude: string, longitude: string): Observable<AirPollution>{
+    return this.http.get<AirPollution>(`http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${latitude}&lon=${longitude}&appid=${environment.openWeather.API_key}`)
+  }
+
+  getHistoricalAirPollution(latitude: string, longitude: string, startUnixTime: number, endUnixTime: number ): Observable<AirPollution>{ //Start/end date (unix time, UTC time zone), e.g. start=1606488670
+    return this.http.get<AirPollution>(`http://api.openweathermap.org/data/2.5/air_pollution/history?lat=${latitude}&lon=${longitude}&start=${startUnixTime}&end=${endUnixTime}&appid=${environment.openWeather.API_key}`)
   }
 
 }
