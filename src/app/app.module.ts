388c2992e16ca3b0a7e19b21e2320e8bb4bc76fa
Ekaterina Brekhova <brekhova.katya@gmail.com>
2022-07-22 12:19:20 +0700

    feat: add city autocomplete in the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CityAutocompleteComponent } from './autocomplete/city-autocomplete.component';
 import { LangToggleComponent } from './lang-toggle/lang-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    CityAutocompleteComponent,
     LangToggleComponent,
   ],
   imports: [
@@ -21,7 +26,9 @@ import { LangToggleComponent } from './lang-toggle/lang-toggle.component';
     FormsModule,
     MatNativeDateModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     MatButtonToggleModule,
+    MatAutocompleteModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
