0de19832ec1cab0104d8add121a6f6e64693d9f2
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-22 11:07:41 +0400

    feat: add navigator get geolocation

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -13,10 +13,20 @@ export class MainPageComponent implements OnInit {
   currentWeather: CurrentWeather = {} as CurrentWeather;
   forecast: Forecast = {} as Forecast;
   cityCoordinates: CityCoordinates = {} as CityCoordinates;
+  crd: any;
 
   constructor(public weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      this.crd = pos.coords;
+      this.weatherService.getForecast(this.crd.latitude, this.crd.longitude, 'ru').subscribe({
+        next: (value) => this.forecast = value,
+        error: (err) => console.log(err),
+        complete: () => console.log(this.forecast)
+     });
+    })
+
     this.weatherService.getCoordinatesByLocationName('Izhevsk', 'RU').subscribe({
       next: (value) => this.cityCoordinates = value,
       error: (err) => console.log(err),
