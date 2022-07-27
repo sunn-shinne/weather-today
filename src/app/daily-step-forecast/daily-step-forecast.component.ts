e7e1fa3ec37d4fe0c9145bcb54c16d786cc082d5
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-27 19:49:06 +0400

    feat: add expanded detail

--- src/app/daily-step-forecast/daily-step-forecast.component.ts
+++ src/app/daily-step-forecast/daily-step-forecast.component.ts
@@ -1,24 +1,33 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { WeatherService } from '../services/weather.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-daily-step-forecast',
   templateUrl: './daily-step-forecast.component.html',
-  styleUrls: ['./daily-step-forecast.component.scss']
+  styleUrls: ['./daily-step-forecast.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class DailyStepForecastComponent implements OnInit {
+  expandedElement!: any;
+  
+  columnsToDisplay: string[] = [];
 
   @HostListener('window:resize', ['$event'])
   onResize() {
-   if(window.innerWidth < 760) {
-    this.columnsToDisplay = ['valid_date','weather'];
-   } else {
-    this.columnsToDisplay = ['valid_date','weather','wind_spd','pres','rh'];
-   }
-}
-
-  columnsToDisplay: string[] = [];
+    if(window.innerWidth < 760) {
+      this.columnsToDisplay = ['valid_date','weather'];
+    } else {
+      this.columnsToDisplay = ['valid_date','weather','wind_spd','pres','rh'];
+    }
+  }
 
   constructor(
     public weatherService: WeatherService,
@@ -26,7 +35,11 @@ export class DailyStepForecastComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
-    this.columnsToDisplay = ['valid_date','weather','wind_spd','pres','rh'];
+    if(window.innerWidth < 760) {
+      this.columnsToDisplay = ['valid_date','weather'];
+    } else {
+      this.columnsToDisplay = ['valid_date','weather','wind_spd','pres','rh'];
+    }
   }
 
   addSignToTemp(temp: number): string {
