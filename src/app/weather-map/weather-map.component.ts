11bad498eba8df4717019ddb8273cf9935078a4d
Ekaterina Brekhova <brekhova.katya@gmail.com>
2022-08-01 12:28:10 +0700

    feat: create weather map

---
+++ src/app/weather-map/weather-map.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { LocationService } from '../services/location.service';
+import { environment } from 'src/environments/environment';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-weather-map',
+  templateUrl: './weather-map.component.html',
+  styleUrls: ['./weather-map.component.scss'],
+})
+export class WeatherMapComponent {
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 12,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }),
+    ],
+    zoom: 10,
+    center: L.latLng(
+      this.locationService.currentLocation.cords.lat,
+      this.locationService.currentLocation.cords.lon
+    ),
+  };
+
+  baseLayers = {
+    Precipitation: L.tileLayer(
+      `https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${environment.openWeather.API_key}`
+    ),
+    Temperature: L.tileLayer(
+      `https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=${environment.openWeather.API_key}`
+    ),
+    'Wind speed': L.tileLayer(
+      `https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=${environment.openWeather.API_key}`
+    ),
+    Clouds: L.tileLayer(
+      `https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${environment.openWeather.API_key}`
+    ),
+    'Sea level pressure': L.tileLayer(
+      `https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?appid=${environment.openWeather.API_key}`
+    ),
+  };
+
+  constructor(public locationService: LocationService) {}
+}
