bb3ec2ad8d18567a833d521fadc912ad34bf3fce
Ekaterina Brekhova <brekhova.katya@gmail.com>
2022-08-05 11:49:28 +0700

    refactor: theme setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,16 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  host: {
+    '[class.theme-morning]': 'hours >= 4 && hours < 12',
+    '[class.theme-afternoon]': 'hours >= 12 && hours < 20',
+    '[class.theme-night]': 'hours >= 20 || hours < 4',
+  },
 })
 export class AppComponent {
   title = 'weather-today';
-  constructor(private elementRef: ElementRef) {
-  }
-
-  ngOnInit(): void {
-  }
-
-  changeBackgroundColor(): any {
-    let time = new Date().getHours();
-    switch (true) {
-      case (time >= 4 && time < 12):
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.backgroundColor = 'var(--morning)';
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.background = 'linear-gradient(180deg, rgba(252,221,240,1) 0%, rgba(212,197,255,1) 66%, rgba(125,182,255,1) 100%)';
-        break;
-      case (time >= 12 && time < 20):
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.backgroundColor = 'var(--afternoon)';
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.background = 'linear-gradient(180deg, rgba(187,211,248,1) 41%, rgba(255,225,165,1) 85%, rgba(255,223,153,1) 100%)';
-        break;
-      case (time >= 20 || time < 4):
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.backgroundColor = 'var(--night)';
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.background = 'linear-gradient(180deg, rgba(186,189,255,1) 12%, rgba(175,170,236,1) 70%, rgba(132,105,172,1) 100%)';
-        break;
-      default:
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.backgroundColor = 'var(--morning)';
-        this.elementRef.nativeElement.ownerDocument
-          .body.style.background = 'linear-gradient(180deg, rgba(252,221,240,1) 0%, rgba(212,197,255,1) 66%, rgba(125,182,255,1) 100%)';
-    }
-  }
+  hours = new Date().getHours();
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit(): void { }
-
-  changeLogo(): any {
-    let time = new Date().getHours();
-    switch (true) {
-      case (time >= 4 && time < 12):
-        return 'morning'
-      case (time >= 12 && time < 20):
-        return 'afternoon'
-      case (time >= 20 || time < 4):
-        return 'night'
-      default:
+export class HeaderComponent {
+  chooseLogoPath(): string {
+    const hours = new Date().getHours();
+    let themeName;
+    if (hours >= 4 && hours < 12) {
+      themeName = 'morning';
+    } else if (hours >= 12 && hours < 20) {
+      themeName = 'afternoon';
+    } else {
+      themeName = 'night';
     }
+    return `../../assets/icons/logo/${themeName}.svg`;
   }
 }
